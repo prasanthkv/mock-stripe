af8c17e4b4fd9ce223bc17608141f241ab401d0e
prakv <prakv@ebay.com>
2017-11-06 21:36:27 -0800

    updated build & version

--- main.go
+++ main.go
@@ -9,15 +9,27 @@ import (
 	"os"
 )
 
+const Version = "mock-1.0.2"
+const Build = "nov-6-2017"
 
+//
+// Main Method
+//
 func main() {
-
 	//
 	route := mux.NewRouter()
+	//
+	// Admin flows
+	//
 	route.HandleFunc("/v1/version", VersionHandler).
 		Methods("GET")
-	route.HandleFunc("/server/exit", ExitHandler).
+	route.HandleFunc("/admin/exit", ExitHandler).
 		Methods("GET")
+
+	//
+	// mocks
+	//
+
 	//auth flow
 	route.HandleFunc("/v1/charges", AuthauthorizeHandler).
 		Methods("POST").Headers("Content-Type", "application/x-www-form-urlencoded")
@@ -27,16 +39,28 @@ func main() {
 	//refund flow
 	route.HandleFunc("/v1/charges/{id}/refunds", RefundsHandler).
 		Methods("POST").Headers("Content-Type", "application/x-www-form-urlencoded")
+
 	//start server
 	log.Fatal(http.ListenAndServe(":8080", route))
 }
 
+//
+// Handle version request
+//
 func VersionHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "{\"version\": \"mock-1.0.1\", \"build_date\":\"nov-5-2017\"}")
-	log.Println("versionn")
+	fmt.Fprintln(w, "{\"version\": \"" + Version + "\", \"build_date\":\"" + Build + "\"}")
+	log.Println("VersionHandler:Init")
+	//write http status
+	w.WriteHeader(200)
 }
 
-
+//
+// Handle Exit request
+//
 func ExitHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "{\"version\": \"mock-1.0.1\", \"exit\":\"OK\"}")
+	log.Println("ExitHandler:Init")
+	//write http status
+	w.WriteHeader(200)
 	os.Exit(3)
 }
